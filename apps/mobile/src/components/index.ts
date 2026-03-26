032399de1afd6a4117c2bdf6ef840b8dacc91d23
97n5nqrbbn-oss <97n5nqrbbn@privaterelay.appleid.com>
2026-03-25 22:49:37 -0400

    Add Button and Card exports to components index

--- apps/mobile/src/components/index.ts
+++ apps/mobile/src/components/index.ts
@@ -3,3 +3,5 @@ export { GarmentCard } from './GarmentCard';
 export { ConfidenceScore } from './ConfidenceScore';
 export { LoadingOverlay } from './LoadingOverlay';
 export { EmptyState } from './EmptyState';
+export { Button } from './ui/Button';
+export { Card } from './ui/Card';
